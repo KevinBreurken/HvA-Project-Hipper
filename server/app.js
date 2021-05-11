4b888e3afd9e804084b436d54553ac5e2cdcdfd4
Kevin Breurken <kevinbreurken@gmail.com>
2021-05-12 00:09:43 +0200

    changed pagination request from POST to GET

--- server/app.js
+++ server/app.js
@@ -155,10 +155,22 @@ app.post("/room_example", (req, res) => {
 
 });
 
-app.post("/caretaker/all", (req, res) => {
+app.get("/caretaker/all", (req, res) => {
+    const maxPerPagination = 2;
+    const currentPaginationOffset = (req.query.paginationPosition - 1) * maxPerPagination;
     db.handleQuery(connectionPool, {
-        query: "SELECT `r`.* FROM `rehabilitator` as `r` INNER JOIN `caretaker` as `c` on `r`.`caretaker_id` = `c`.`caretaker_id` INNER JOIN `user` as `u` on `u`.`id` = `c`.`user_id` WHERE `u`.`id` = ?",
-        values: [req.body.userID]
+        query: "SELECT `r`.* FROM `rehabilitator` as `r` INNER JOIN `caretaker` as `c` on `r`.`caretaker_id` = `c`.`caretaker_id` INNER JOIN `user` as `u` on `u`.`id` = `c`.`user_id` WHERE `u`.`id` = ? LIMIT ? OFFSET ?",
+        values: [req.query.userID, maxPerPagination, currentPaginationOffset]
+    }, (data) => {
+        res.status(httpOkCode).json(data);
+    }, (err) => res.status(badRequestCode).json({reason: err}))
+})
+
+app.get("/caretaker/all/count", (req, res) => {
+    console.log(req.query.userID)
+    db.handleQuery(connectionPool, {
+        query: "SELECT Count(*) FROM `rehabilitator` as `r` INNER JOIN `caretaker` as `c` on `r`.`caretaker_id` = `c`.`caretaker_id` INNER JOIN `user` as `u` on `u`.`id` = `c`.`user_id` WHERE `u`.`id` = ?",
+        values: [req.query.userID]
     }, (data) => {
         res.status(httpOkCode).json(data);
     }, (err) => res.status(badRequestCode).json({reason: err}))
--- src/assets/js/repositories/caretakerRepository.js
+++ src/assets/js/repositories/caretakerRepository.js
@@ -8,12 +8,15 @@ class CaretakerRepository {
     }
 
     async getAllRehab(userID, paginationPosition) {
+        // return await networkManager
+        //     .doRequest(`${this.route}/all`, {"userID": userID, "paginationPosition": paginationPosition}, "POST");
         return await networkManager
-            .doRequest(`${this.route}/all`, {"userID": userID, "paginationPosition": paginationPosition}, "POST");
+            .doRequest(`${this.route}/all/`, {"userID": userID, "paginationPosition": paginationPosition}, "GET");
     }
 
     async getRehabCount(userID) {
+        const page = 1;
         return await networkManager
-            .doRequest(`${this.route}/all/count`, {"userID": userID}, "POST");
+            .doRequest(`${this.route}/all/count`, {"userID": userID}, "GET", false);
     }
 }
